f91954781c0a6625bf969aa87b2f50bb6c7cd094
Ruben Arutyunov <ru.arutyunov@gmail.com>
2021-04-16 16:32:42 -0500

    Adding formatting for weather command

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -1,5 +1,18 @@
 const {sendMessage} = require('../helpers/messageSender');
 const dataHandler = require('../handlers/weatherHandler');
+const lodash = require('lodash');
+
+const _mapFields = (weatherData) => {
+  const fields = [];
+  for (const [k, v] of Object.entries(weatherData)) {
+    mappedData = {
+      name: lodash.startCase(k),
+      value: v,
+    };
+    fields.push(mappedData);
+  }
+  return fields;
+};
 
 exports.run = async (message, args) => {
   if (args.length < 1) {
@@ -8,6 +21,20 @@ exports.run = async (message, args) => {
   }
   const city = args[0];
   const data = await dataHandler(city);
+  const image = {
+    url: data.weatherIcon,
+  };
+  delete data.weatherIcon;
+
+  const fields = _mapFields(data);
+
+  const helpEmbed = {
+    color: 0x0099ff,
+    title: 'Current Weather',
+    fields,
+    image,
+  };
+  sendMessage(message, {embed: helpEmbed});
 };
 
 exports.conf = {
--- src/handlers/weatherHandler.js
+++ src/handlers/weatherHandler.js
@@ -1,13 +1,32 @@
 const {getCurrentWeather} = require('../repositories/weatherRepository');
 
+const _getWindSpeedAndDir = (data) => {
+  return data.wind_speed + 'mph ' + data.wind_dir;
+};
+
+const _flattenWeatherDescriptions = (data) => {
+  return data.weather_descriptions.join(', ');
+};
+
+const _mapWeatherResponse = (loc, current) => {
+  return {
+    asOfDateTime: loc.localtime,
+    location: loc.name,
+    temperature: current.temperature,
+    weatherIcon: current.weather_icons[0],
+    weatherDescriptions: _flattenWeatherDescriptions(current),
+    wind: _getWindSpeedAndDir(current),
+    feelsLike: current.feelslike,
+  };
+};
+
 module.exports = async (city) => {
   try {
     const data = await getCurrentWeather(city);
     const body = JSON.parse(data.body);
-
-    return {
-      body,
-    };
+    const locationData = body.location;
+    const currentWeatherData = body.current;
+    return _mapWeatherResponse(locationData, currentWeatherData);
   } catch (e) {
     console.log(e);
   }
